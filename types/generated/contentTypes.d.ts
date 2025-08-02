231c1ec4d5b1c83a9a2de81363d7381ce828b1c5
kervisvasquez1993 <cowin0809@gmail.com>
2025-08-02 09:19:36 -0300

    update campo in project

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -922,7 +922,7 @@ export interface ApiImagenEventoImagenEvento extends Schema.CollectionType {
     titulo: Attribute.String & Attribute.Required;
     descriptions: Attribute.RichText;
     imagenes: Attribute.Media<'images' | 'files' | 'videos' | 'audios', true>;
-    imgs: Attribute.Media<'images' | 'videos', true>;
+    videosimple: Attribute.Media<'videos', true>;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
